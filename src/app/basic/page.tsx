57e062457c3c99f656ef6698db94818c17a46530
HeavenOSK <heavenosk@gmail.com>
2025-05-16 23:11:35 +0900

    BasicコンポーネントのUIを改善し、複数のボックスをアニメーションで表示するように変更

--- src/app/basic/page.tsx
+++ src/app/basic/page.tsx
@@ -4,10 +4,17 @@ import { motion } from "motion/react";
 
 function Basic() {
 	return (
-		<div className="flex h-screen w-screen flex-col items-center justify-center gap-4">
-			<motion.div animate={{ x: 100 }}>
-				<div className="h-24 w-24 bg-red-500" />
-			</motion.div>
+		<div className="flex h-screen w-screen flex-col items-center gap-36 overflow-y-auto p-16">
+			{[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15].map((i) => (
+				<motion.div
+					key={i}
+					initial={false}
+					animate={{ scale: 1 }}
+					transition={{ duration: 2 }}
+				>
+					<div className="h-24 w-24 bg-red-500" />
+				</motion.div>
+			))}
 		</div>
 	);
 }
